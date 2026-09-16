ebb0760593a30d5e15fd6a7b2bb8d2540671fb6d
StanneLLC <189099+StanneLLC@users.noreply.github.com>
2026-09-16 07:33:01 -0400

    Create NumberPushSolution.cs

---
+++ NumberPushSolution.cs
@@ -0,0 +1,61 @@
+﻿namespace NovaWright.NumberPush.LevelGenerator
+{
+    public class NumberPushSolution
+    {
+        public bool IsSolved { get; set; }
+
+        public int MinimumPushes { get; set; }
+
+        public List<NumberPushSolutionStep> Steps { get; set; } =
+            new();
+
+        public Dictionary<int, int> CratePushCounts { get; set; } =
+            new();
+
+        public Dictionary<int, int> CrateFirstPushNumbers { get; set; } =
+            new();
+
+        public Dictionary<int, int> CrateLastPushNumbers { get; set; } =
+            new();
+
+        public Dictionary<int, Point> CrateGoalPositions { get; set; } =
+    new();
+
+        public int CratesMoved
+        {
+            get
+            {
+                return CratePushCounts.Count;
+            }
+        }
+    }
+
+    public class NumberPushSolutionStep
+    {
+        public int PushNumber { get; set; }
+
+        public int CrateIndex { get; set; }
+
+        public int CrateNumber
+        {
+            get
+            {
+                return CrateIndex + 1;
+            }
+        }
+
+        public Point PlayerStart { get; set; }
+
+        public Point PlayerPushPosition { get; set; }
+
+        public Point CrateStart { get; set; }
+
+        public Point CrateEnd { get; set; }
+
+        public Point Direction { get; set; }
+
+        public int Distance { get; set; }
+
+        public List<Point> PlayerPath { get; set; } = new();
+    }
+}
